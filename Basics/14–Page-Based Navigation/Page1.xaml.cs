f906804a2b32101efc8f7ac73e3bb46f95337176
jh397688 <52513981+jh397688@users.noreply.github.com>
2021-03-13 21:10:53 +0900

    Title : 14–Page-Based Navigation
    Source : https://wpf.2000things.com/2010/07/26/14-page-based-navigation/
    
    WPF는 페이지 간의 탐색 기능을 제공합니다.

---
+++ Basics/14–Page-Based Navigation/Page1.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace _14_Page_Based_Navigation
+{
+    /// <summary>
+    ///
+    /// Source : https://wpf.2000things.com/2010/07/26/14-page-based-navigation/
+    /// 
+    /// WPF는 페이지 간의 탐색 기능을 제공합니다.
+    /// 
+    /// </summary>
+    public partial class Page1 : Page
+    {
+        public Page1()
+        {
+            InitializeComponent();
+        }
+    }
+}
