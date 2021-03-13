cc64c2cf9d28633ed916eff9d52db42ff93d3681
jh397688 <52513981+jh397688@users.noreply.github.com>
2021-03-13 20:26:38 +0900

    Title : 11-Command
    WPF는 Command 기능을 지원합니다.
    Command는 WPF GUI 구성과 독립적으로 특정 작업을 수행하는 개체입니다.
    
    Command의 주요 목적은 여러 컨트롤에서 공통된 코드를
    개별 컨트롤의 이벤트 처리기에서 메인 처리 위치로 이동합니다.
    이를 통해 여러 컨트롤이 동일한 Command 를 재사용 가능합니다.
    Command 를 사용할 컨트롤은 메인 처리 위치에 바인딩 걸어 사용합니다.
    
    Source : https://wpf.2000things.com/2010/07/23/11-commands/

--- Basics/11-Commands/MainWindow.xaml.cs
+++ Basics/11-Commands/MainWindow.xaml.cs
@@ -16,7 +16,17 @@ using System.Windows.Shapes;
 namespace _11_Commands
 {
     /// <summary>
-    /// MainWindow.xaml에 대한 상호 작용 논리
+    /// 
+    /// Source : https://wpf.2000things.com/2010/07/23/11-commands/
+    /// 
+    /// WPF는 Command 기능을 지원합니다.
+    /// Command는 WPF GUI 구성과 독립적으로 특정 작업을 수행하는 개체입니다.
+    /// 
+    /// Command의 주요 목적은 여러 컨트롤에서 공통된 코드를
+    /// 개별 컨트롤의 이벤트 처리기에서 메인 처리 위치로 이동합니다.
+    /// 이를 통해 여러 컨트롤이 동일한 Command 를 재사용 가능합니다.
+    /// Command 를 사용할 컨트롤은 메인 처리 위치에 바인딩 걸어 사용합니다.
+    /// 
     /// </summary>
     public partial class MainWindow : Window
     {
